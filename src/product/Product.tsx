b08a782493636a447c5587f4583b4ef32c186d9d
Anish Gyawali <anish.gyawali7@gmail.com>
2023-10-18 19:47:29 -0500

    updated product page

--- src/product/Product.tsx
+++ src/product/Product.tsx
@@ -1,11 +1,23 @@
-import './Product.css'
+import {createStyles} from "@mantine/core"
 
 const Product=()=>{
+    const {classes}= useStyles();
     return(
-        <div className="product_container">
+        <div className={classes.product_container}>
         <p>Create Product</p>
+        
         </div>
     )
 }
 
 export default Product;
+
+const useStyles= createStyles(()=>{
+    return{
+        product_container:{
+            display:"flex",
+            justifyContent:"center",
+            marginTop:"3rem"
+        }
+    }
+})
